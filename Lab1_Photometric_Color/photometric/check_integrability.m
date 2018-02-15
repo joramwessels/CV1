d4a8bad404916fa8d04b079977dc2eed875ede39
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-02-15 21:22:08 +0100

    completed second subtask

--- Lab1_Photometric_Color/photometric/check_integrability.m
+++ Lab1_Photometric_Color/photometric/check_integrability.m
@@ -5,16 +5,21 @@ function [ p, q, SE ] = check_integrability( normals )
 %   q : df / dy
 %   SE : Squared Errors of the 2 second derivatives
 
+[h, w, ~] = size(normals);
+
 % initalization
-p = zeros(size(normals));
-q = zeros(size(normals));
-SE = zeros(size(normals));
+p = zeros(h,w);
+q = zeros(h,w);
+SE = zeros(h,w);
 
 % ========================================================================
 % YOUR CODE GOES HERE
 % Compute p and q, where
 % p measures value of df / dx
 % q measures value of df / dy
+
+p = normals(:,:,1)./normals(:,:,3);
+q = normals(:,:,2)./normals(:,:,3);
 
 
 % ========================================================================
@@ -31,6 +36,20 @@ q(isnan(q)) = 0;
 % approximate second derivate by neighbor difference
 % and compute the Squared Errors SE of the 2 second derivatives SE
 
+
+py = zeros(size(p));
+qx = zeros(size(q));
+
+% Estimate second-order derivatives using central difference
+for x=2:w-1
+    qx(:,x) = (q(:,x+1) - q(:,x-1))/2;
+end
+
+for y=2:h-1
+    py(y,:) = (q(y+1,:) - q(y-1,:))/2;
+end 
+
+SE = (py - qx)^2;
 
 % ========================================================================
 
--- Lab1_Photometric_Color/photometric/construct_surface.m
+++ Lab1_Photometric_Color/photometric/construct_surface.m
@@ -29,9 +29,28 @@ switch path_type
 
        
         % =================================================================
-               
+        
+        for y=2:h
+            height_map(y,1) = height_map(y-1,1) + q(y,1);
+        end
+        
+        for y=1:h
+            for x=2:w
+                height_map(y,x) = height_map(y,x-1) + p(y,x);
+            end
+        end
+        
     case 'row'
         
+        for x=2:w
+            height_map(1,x) = height_map(1,x-1) + p(1,x);
+        end
+        
+        for x=1:w
+            for y=2:h
+                height_map(y,x) = height_map(y-1,x) + q(y,x);
+            end
+        end
         % =================================================================
         % YOUR CODE GOES HERE
         
@@ -43,7 +62,32 @@ switch path_type
         % =================================================================
         % YOUR CODE GOES HERE
 
+
+        height_map_col = zeros(h, w);
+        height_map_row = zeros(h, w);
+
+
+        for y=2:h
+            height_map_col(y,1) = height_map_col(y-1,1) + q(y,1);
+        end
         
+        for y=1:h
+            for x=2:w
+                height_map_col(y,x) = height_map_col(y,x-1) + p(y,x);
+            end
+        end
+        
+        for x=2:w
+            height_map_row(1,x) = height_map_row(1,x-1) + p(1,x);
+        end
+        
+        for x=1:w
+            for y=2:h
+                height_map_row(y,x) = height_map_row(y-1,x) + q(y,x);
+            end
+        end      
+        
+        height_map = (height_map_col + height_map_row)./2;
         % =================================================================
 end
 
