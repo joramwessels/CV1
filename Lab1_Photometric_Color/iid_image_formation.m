3833d22c5fc499e8bda4aa3caf5a78853788b445
joramwessels <joram.wessels@outlook.com>
2018-02-21 11:49:29 +0100

    Question 3 finished

--- Lab1_Photometric_Color/iid_image_formation.m
+++ Lab1_Photometric_Color/iid_image_formation.m
@@ -1,6 +1,6 @@
-original = imread('intrinsic_images/ball.png');
-reflect  = imread('intrinsic_images/ball_reflectance.png');
-shading  = imread('intrinsic_images/ball_shading.png');
+original = im2double(imread('intrinsic_images/ball.png'));
+reflect  = im2double(imread('intrinsic_images/ball_reflectance.png'));
+shading  = im2double(imread('intrinsic_images/ball_shading.png'));
 
 result = reflect .* shading;
 
--- Lab1_Photometric_Color/recoloring.m
+++ Lab1_Photometric_Color/recoloring.m
@@ -1,10 +1,31 @@
-original = imread('intrinsic_images/ball.png');
-reflect  = imread('intrinsic_images/ball_reflectance.png');
-shading  = imread('intrinsic_images/ball_shading.png');
+original = im2double(imread('intrinsic_images/ball.png'));
+reflect  = im2double(imread('intrinsic_images/ball_reflectance.png'));
+shading  = im2double(imread('intrinsic_images/ball_shading.png'));
 
-mask = zeros([266, 480, 3]);
+mask = im2double(zeros([266, 480, 3]));
 mask(:, :, 2) = 1.0;
-
 pure_green = reflect .* mask;
+pure_green(pure_green>0) = 1;
 
-imshow(pure_green)
+green_ball = pure_green .* shading;
+
+mask = im2double(zeros([266, 480, 3]));
+mask(:, :, 1) = 1.0;
+mask(:, :, 3) = 1.0;
+magenta = reflect .* mask;
+magenta(magenta>0) = 1;
+
+magenta_ball = magenta .* shading;
+
+figure
+subplot(1,3,1)
+imshow(original)
+title('Original image')
+
+subplot(1,3,2)
+imshow(green_ball)
+title('Pure green image')
+
+subplot(1,3,3)
+imshow(magenta_ball)
+title('Magenta image')
