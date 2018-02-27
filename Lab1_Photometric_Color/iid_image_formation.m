f2d84e5695c2e635fdac1628af96bbcb578bb12c
joramwessels <joram.wessels@outlook.com>
2018-02-27 09:42:11 +0100

    Assignment 2

--- Lab1_Photometric_Color/iid_image_formation.m
+++ Lab1_Photometric_Color/iid_image_formation.m
@@ -5,3 +5,20 @@ shading  = im2double(imread('intrinsic_images/ball_shading.png'));
 result = reflect .* shading;
 
 imshow(result)
+
+figure
+subplot(2,2,1)
+imshow(original)
+title('Original image')
+
+subplot(2,2,2)
+imshow(reflect)
+title('The reflection component')
+
+subplot(2,2,3)
+imshow(shading)
+title('The shading component')
+
+subplot(2, 2, 4)
+imshow(result)
+title('The element wise multiplication')
