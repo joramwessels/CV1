73ac5b97bfe4ddea0b1da60dc39e0a0347c45043
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-02-27 16:42:41 +0100

    finished task 6 & beginning of task 7

--- Lab2/Part-1_ Image Enhancement/myPSNR.m
+++ Lab2/Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,8 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
-
+I_max = max(orig_image(:));
+[M,N,C] = size(orig_image);
+sq_diff = (orig_image - approx_image).^2;
+MSE = sum(sq_diff(:))/(M*N*C);
+PSNR = 10 * log10(I_max^2/MSE);
 end
 
