5c260b104ca299f35fdc8323775dad7281a58873
joramwessels <joram.wessels@outlook.com>
2018-02-27 12:47:05 +0100

    Gaussian convolutions

--- Lab2/Part-1_ Image Enhancement/gauss1D.m
+++ Lab2/Part-1_ Image Enhancement/gauss1D.m
@@ -1,7 +1,13 @@
 function G = gauss1D( sigma , kernel_size )
-    G = zeros(1, kernel_size);
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
-    %% solution
+    fraction = ones(1, kernel_size)*(1/sigma*sqrt(2*pi));
+    exponent = ones(1, kernel_size);
+    for i = 1:kernel_size
+        x = i -1 -floor(kernel_size/2);
+        exponent(1, i) = exp(-x^2/(2*sigma^2));
+    end
+    G = fraction .* exponent;
+    G = G / sum(G);
 end
