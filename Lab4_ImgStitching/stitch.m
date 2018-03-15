a07f02bb58728d5d7482d78d377ddf859f51ac0a
joramwessels <joram.wessels@outlook.com>
2018-03-15 17:28:13 +0100

    blablabla

--- Lab4_ImgStitching/stitch.m
+++ Lab4_ImgStitching/stitch.m
@@ -1,3 +1,6 @@
 function result = stitch(im1, im2)
-    
+    [f1, f2, matches, scores] = keypoint_matching(im1, im2);
+    bt = RANSAC(f1, f2, matches, scores);
+    M = [bt(1), bt(2); bt(3), bt(4)];
+    T = [bt(5); bt(6)];
 end
