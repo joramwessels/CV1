baa37af0a6500051d2381ca176ae3541cdcb46a0
joramwessels <joram.wessels@outlook.com>
2018-03-16 19:40:29 +0100

    Still incorrect output

--- Lab4_ImgStitching/stitch.m
+++ Lab4_ImgStitching/stitch.m
@@ -1,6 +1,6 @@
 function stitch = stitch(im1, im2)
     [xy1, xy2] = keypoint_matching(im1, im2);
-    bt = RANSAC(xy1, xy2, 10, 50);
+    bt = RANSAC(xy1, xy2, 100, 50);
     Transform = zeros([3, 3]);
     M = [bt(1), bt(2); bt(3), bt(4)];
     T = [bt(5); bt(6); 1];
@@ -14,15 +14,18 @@ function stitch = stitch(im1, im2)
     % printing the transformed image on the canvas
     im2 = imtransform(im2, maketform('affine', Transform));
     [h, w] = size(im2);
-    sh = start2(1);
-    sw = start2(2);
-    stitch(sh:sh+h-1, sw:sw+w-1) = im2;
+    T = round(T);
+    stitch((1:h)+T(1), (1:w)+T(2)) = im2;
+    %sh = start2(1);
+    %sw = start2(2);
+    %stitch(sh:sh+h-1, sw:sw+w-1) = im2;
     
     % printing the unchanged image onto the canvas
     [h, w] = size(im1);
-    sh = start1(1);
-    sw = start1(2);
-    stitch(sh:(sh+h-1), sw:(sw+w-1)) = im1;
+    stitch(1:h, 1:w) = im1;
+    %sh = start1(1);
+    %sw = start1(2);
+    %stitch(sh:(sh+h-1), sw:(sw+w-1)) = im1;
     
     stitch = stitch / max(max(stitch));
     imshow(stitch);
@@ -36,16 +39,16 @@ function [start1, start2, max_dims] = get_max_dimensions(T, im1, im2)
     % max_dims tells the dimensions of the canvas itself.
     [h, w] = size(im2);
     corners = zeros([4, 3]);
-    corners(1, :) = [0, 0, 1] * T;
-    corners(2, :) = [0, w, 1] * T;
-    corners(3, :) = [h, 0, 1] * T;
+    corners(1, :) = [1, 1, 1] * T;
+    corners(2, :) = [1, w, 1] * T;
+    corners(3, :) = [h, 1, 1] * T;
     corners(4, :) = [h, w, 1] * T;
     mins = min(corners);
     maxs = max(corners);
     max_dims = round(cat(1, mins(1:2), maxs(1:2)));
     
-    start1 = zeros([1, 2]);
-    start2 = zeros([1, 2]);
+    start1 = ones([1, 2]);
+    start2 = ones([1, 2]);
     
     if max_dims(1, 1) < 1
         start1(1) = round(-max_dims(1, 1));
