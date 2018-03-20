43adda353baf21fe8062ff623b4fd9f0714c521c
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-03-20 14:51:49 +0100

    bug fixes, changed output to histogram object'

--- Lab5_Project/part1/get_histogram.m
+++ Lab5_Project/part1/get_histogram.m
@@ -1,11 +1,19 @@
-function norm_histogram = get_histogram( im, vocab )
+function h = get_histogram( im, vocab )
 % GET_HISTOGRAM: encodes image in histogram of visual words
 % INPUT:
 %   im: image array
 %   vocab: mxn array with m=#features and n=sift dims
 % OUTPUT:
-%   histogram: visual word counts for given image
+%   histogram: histogram object
     
+%   Image preprocessing
+    if not (size(im, 3) == 1)
+        im = rgb2gray(im) ;
+    end
+    if not(isa(im, 'single'))
+        im = single(im) ;
+    end
+
 %   Detect SIFT features for image
     [F_im, D_im] = vl_sift(im) ;
     n_features = size(F_im, 2) ;
@@ -15,7 +23,7 @@ function norm_histogram = get_histogram( im, vocab )
     n_clusters = size(vocab, 1) ; 
     
 %   Initialize histogram counts
-    histogram = zeros(1, n_clusters) ;
+    assignments = zeros(1, n_features) ;
     
     % Assign cluster means for each feature descriptor
     for i=1:n_features
@@ -26,23 +34,22 @@ function norm_histogram = get_histogram( im, vocab )
         % Find closest cluster mean
         for j=1:n_clusters
             
-            descriptor_i = D_im(:, i) ;
+            descriptor_i = double(D_im(:, i)) ;
             cc_j = cluster_centroids(j, :) ;
             
             % Calculate euclidian distance between cluster mean and
             % descriptor
-            dist = pdist([descriptor_i ; cc_j], 'euclidian') ;
+            dist = pdist([descriptor_i' ; cc_j], 'squaredeuclidean') ;
             
             if dist < min_dist
                 assigned_cluster = j ;
                 min_dist = dist ;
             end
         end
+        assignments(:, i) = assigned_cluster ;
         
-        % Add count for cluster mean to histogram
-        histogram(:, j) = histogram(:, j) + 1 ;
     end
     
-% Normalize histogram
-norm_histogram = histogram ./ n_features ; 
+    % Create histogram for cluster assignments
+    h = histogram(assignments, 'Normalization', 'probability') ;
 end
